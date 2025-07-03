ceb73fa67f14c3f57e33e80ceb5d0afe3c6542c0
mohamedelsayed0001 <mohamedelsayyed234@gmail.com>
2025-07-03 23:22:32 +0300

    Lasso reg & Linear reg fix

--- ML/Supervised/Regression/LassoRegression.py
+++ ML/Supervised/Regression/LassoRegression.py
@@ -1,20 +1,22 @@
 import numpy as np
 
-class LinearRegression:
+class LassoRegression:
 
     """
-    Multivariate Linear Regression using Gradient Descent.
+    Multivariate Lasso Regression using Gradient Descent.
 
-    This class supports training on multiple features to fit a model of the form:
+    This class supports training on multiple features with L1 regularization (Lasso),
+    fitting a model of the form:
         y = w1*x1 + w2*x2 + ... + wn*xn + b
 
     Methods:
-        - train(X, Y): Fits weights and bias using gradient descent.
+        - train(X, Y): Trains the model using L1-regularized gradient descent.
         - predict(X): Predicts target values for input features.
         - evaluate(X, Y): Computes Mean Squared Error on test data.
     """
      
-    def __init__(self,learning_rate=0.01,epochs=1000):
+    def __init__(self,learning_rate=0.01,epochs=1000,Lambda = 2):
+        self.Lambda = Lambda
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.coefficients = None
@@ -27,13 +29,15 @@ class LinearRegression:
         b = 0.0
 
         for _ in range(self.epochs):
-            y_bar = np.dot(X, w) + b
+            y_bar = np.dot(X, w) + b  
             error = y_bar - Y
 
             dw = (2 / n_samples) * np.dot(X.T, error)
             db = (2 / n_samples) * np.sum(error)
 
-            w -= self.learning_rate*dw
+            dw += self.Lambda * np.sign(w)
+
+            w -= self.learning_rate*dw  
             b -= self.learning_rate*db
         
         self.coefficients = w 
@@ -44,5 +48,5 @@ class LinearRegression:
     
     def evaluate(self,X,Y):
         y_bar = self.predict(X)
-        mse = np.mean((y_bar - Y) ** 2)
+        mse = np.mean((y_bar-Y)**2)
         return mse
